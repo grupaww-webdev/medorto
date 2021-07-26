4bd0d992dcfd3ee274ad758f927506d5080a4e19
Jakub Zimoń <jakub.zimon@gmail.com>
2021-07-26 13:19:50 +0200

    Add listeners

---
+++ src/Controller/Shop/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Shop;
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+
+final class ProductController extends ResourceController
+{
+    public function newestAction()
+    {
+        dd('test');
+    }
+}
