1d4d4d3a674d1e448011dfa5ddde07dcb6764da3
Marcin <55300639+mgalej@users.noreply.github.com>
2023-05-23 23:41:26 +0200

    Discount for nty product

---
+++ src/Form/Type/Action/EnProductDiscountConfigurationType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form\Type\Action;
+
+use Sylius\Bundle\PromotionBundle\Form\DataTransformer\PercentFloatToLocalizedStringTransformer;
+use Sylius\Bundle\PromotionBundle\Form\Type\PromotionFilterCollectionType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Type;
+
+class EnProductDiscountConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('percentage', PercentType::class, [
+                'label' => 'sylius.form.promotion_action.percentage_discount_configuration.percentage',
+                'constraints' => [
+                    new NotBlank(['groups' => ['sylius']]),
+                    new Type(['type' => 'numeric', 'groups' => ['sylius']]),
+                    new Range([
+                        'min' => 0,
+                        'max' => 1,
+                        'notInRangeMessage' => 'sylius.promotion_action.percentage_discount_configuration.not_in_range',
+                        'groups' => ['sylius'],
+                    ]),
+                ],
+            ])
+            ->add('enty', IntegerType::class, [
+                'label' => 'sylius.form.promotion_action.percentage_discount_configuration.enty',
+                'constraints' => [
+                    new NotBlank(['groups' => ['sylius']]),
+                    new Type(['type' => 'numeric', 'groups' => ['sylius']]),
+                    new Range([
+                        'min' => 2,
+                        'max' => 10,
+                        'notInRangeMessage' => 'sylius.promotion_action.percentage_discount_configuration.not_in_range',
+                        'groups' => ['sylius'],
+                    ]),
+                ],
+            ])
+        ;
+        $builder->get('percentage')->resetViewTransformers()->addViewTransformer(new PercentFloatToLocalizedStringTransformer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_promotion_action_cheapest_product_discount_configuration';
+    }
+}
---
+++ src/Promotion/Action/EnProductDiscountPromotionActionCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Promotion\Action;
+
+use App\Entity\Order\OrderItemInterface;
+use App\Entity\Product\ProductVariant;
+use App\Form\Type\Action\EnProductDiscountConfigurationType;
+use Sylius\Component\Core\Distributor\ProportionalIntegerDistributorInterface;
+use Sylius\Component\Core\Promotion\Action\DiscountPromotionActionCommand;
+use Sylius\Component\Core\Promotion\Applicator\UnitsPromotionAdjustmentsApplicatorInterface;
+use Sylius\Component\Order\Model\OrderInterface;
+use Sylius\Component\Promotion\Model\PromotionInterface;
+use Sylius\Component\Promotion\Model\PromotionSubjectInterface;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class EnProductDiscountPromotionActionCommand extends DiscountPromotionActionCommand
+{
+    const TYPE = 'cheapest_item_discount';
+
+    /**
+     * @var ProportionalIntegerDistributorInterface
+     */
+    private $proportionalDistributor;
+
+    /**
+     * @var UnitsPromotionAdjustmentsApplicatorInterface
+     */
+    private $unitsPromotionAdjustmentsApplicator;
+
+    /**
+     * @param ProportionalIntegerDistributorInterface $proportionalIntegerDistributor
+     * @param UnitsPromotionAdjustmentsApplicatorInterface $unitsPromotionAdjustmentsApplicator
+     */
+    public function __construct(
+        ProportionalIntegerDistributorInterface      $proportionalIntegerDistributor,
+        UnitsPromotionAdjustmentsApplicatorInterface $unitsPromotionAdjustmentsApplicator
+    )
+    {
+        $this->proportionalDistributor = $proportionalIntegerDistributor;
+        $this->unitsPromotionAdjustmentsApplicator = $unitsPromotionAdjustmentsApplicator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(PromotionSubjectInterface $subject, array $configuration, PromotionInterface $promotion): bool
+    {
+        if (!$subject instanceof OrderInterface) {
+            throw new UnexpectedTypeException($subject, OrderInterface::class);
+        }
+
+        $items = $subject->getItems();
+        $itemsTotals = [];
+
+        $discountTotals = 0;
+
+        /** @var OrderItemInterface $item */
+        foreach ($items as $item) {
+            $itemsTotals[] = $item->getTotal();
+
+            $discountTotals += $this->calculateDiscount($item, $configuration);
+        }
+
+        $splitPromotion = $this->proportionalDistributor->distribute($itemsTotals, -1 * $discountTotals);
+        $this->unitsPromotionAdjustmentsApplicator->apply($subject, $promotion, $splitPromotion);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigurationFormType()
+    {
+        return EnProductDiscountConfigurationType::class;
+    }
+
+    protected function isConfigurationValid(array $configuration): void
+    {
+        // TODO: Implement isConfigurationValid() method.
+    }
+
+    protected function calculateDiscount(OrderItemInterface $item, array $configuration): int
+    {
+        $percentage = $configuration['percentage'];
+        $enty = $configuration['enty'];
+
+        $price = $item->getUnitPrice();
+        $quantity = $item->getQuantity();
+
+        if ($quantity > $enty) {
+            $floor = floor($quantity / $enty);
+            $discount = $price - ($price * $percentage);
+
+            return floor($floor * $discount);
+        }
+
+        return 0;
+    }
+}
