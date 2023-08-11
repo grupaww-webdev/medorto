0ea602ea2f84914bf5ca08fd78a71e227f699789
Marcin <55300639+mgalej@users.noreply.github.com>
2023-08-11 21:19:59 +0200

    'work'

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -37,10 +37,12 @@ final class ProductRepository extends BaseProductRepository implements
         ChannelInterface $channel,
         string $locale,
         int $limit = self::DEFAULT_LIMIT
-    ): array {
-        return $this->getRefundQueryBuilder($channel, $locale)
-            ->getQuery()
-            ->getResult();
+    ): Pagerfanta
+    {
+        return $this->getPaginator($this->getRefundQueryBuilder($channel, $locale)
+
+            ->setMaxResults($limit)
+            );
     }
 
     public function findLatestByChannel(
@@ -184,3 +186,77 @@ final class ProductRepository extends BaseProductRepository implements
     }
 
 }
+
+
+//final class ProductRepositoryl extends BaseProductRepository
+//{
+//    private const DEFAULT_LIMIT = 12;
+//
+//    public function findAllByBestsellers(
+//        ChannelInterface $channel,
+//        string $locale,
+//        int $limit = self::DEFAULT_LIMIT
+//    ): array {
+//        return $this->getBestsellersQueryBuilder($channel, $locale)
+//            ->setMaxResults($limit)
+//            ->getQuery()
+//            ->getResult();
+//    }
+//
+//    public function findAllByBestsellersPaginator(
+//        ChannelInterface $channel,
+//        string $locale
+//    ): Pagerfanta {
+//        return $this->getPaginator($this->getBestsellersQueryBuilder($channel, $locale));
+//    }
+//
+//    public function findAllByCategoryPaginator(ChannelInterface $channel, string $code): Pagerfanta
+//    {
+//        return $this->getPaginator($this->getByCategoryQueryBuilder($channel, $code));
+//    }
+//
+//    public function findAllByCategory(ChannelInterface $channel, string $code, int $count): array
+//    {
+//        return $this->getByCategoryQueryBuilder($channel, $code)
+//            ->setMaxResults($count)
+//            ->getQuery()
+//            ->getResult();
+//    }
+//
+//    public function findLatestByChannelPaginator(ChannelInterface $channel, string $locale): Pagerfanta
+//    {
+//        return $this->getPaginator($this->createQueryBuilder('o')
+//            ->addSelect('translation')
+//            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+//            ->andWhere(':channel MEMBER OF o.channels')
+//            ->andWhere('o.enabled = true')
+//            ->addOrderBy('o.createdAt', 'DESC')
+//            ->setParameter('channel', $channel)
+//            ->setParameter('locale', $locale));
+//    }
+//
+//    private function getBestsellersQueryBuilder(ChannelInterface $channel, string $locale): QueryBuilder
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->addSelect('translation')
+//            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+//            ->andWhere(':channel MEMBER OF o.channels')
+//            ->andWhere('o.enabled = true')
+//            ->setParameter('channel', $channel)
+//            ->setParameter('locale', $locale);
+//    }
+//
+//    private function getByCategoryQueryBuilder(ChannelInterface $channel, string $code): QueryBuilder
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->innerJoin('o.channels', 'channel')
+//            ->andWhere('o.enabled = true')
+//            ->andWhere('channel = :channel')
+//            ->innerJoin('o.productTaxons', 'productTaxons')
+//            ->addOrderBy('productTaxons.position', 'asc')
+//            ->innerJoin('productTaxons.taxon', 'taxon')
+//            ->andWhere('taxon.code = :code')
+//            ->setParameter('code', $code)
+//            ->setParameter('channel', $channel);
+//    }
+//}
---
+++ src/Twig/PromotionExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Twig;
+
+use App\Entity\Product\ProductTaxon;
+use Doctrine\ORM\EntityManagerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class PromotionExtension extends AbstractExtension
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('isPromotion', [$this, 'isPromotion']),
+        ];
+    }
+
+    public function isPromotion()
+    {
+        /** @var ProductTaxon $taxon */
+        $taxon =  $this->entityManager->getRepository(ProductTaxon::class)->findBy(['taxon' => 27]);
+
+        return count($taxon) < 0;
+    }
+}
